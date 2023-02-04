80c0f4e5edd799ea0f18bc37f7dea1f40cf5ef1a
Timur <timur98nt@gmail.com>
2023-02-04 19:35:23 +0500

    First commit

---
+++ src/Zookeeper.java
@@ -0,0 +1,11 @@
+public class Zookeeper {
+    public static void main(String[] args) {
+        String output = """
+            I love animals!
+            Let's check on the animals...
+            The deer looks fine.
+            The bat looks happy.
+            The lion looks healthy.""";
+        System.out.println(output);
+    }
+}
